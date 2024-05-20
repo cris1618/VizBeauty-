8749225bd7ae057d10f2aaa2412a9d8cc3d26d04
Cristian Del Gobbo <cristiandelgobbo87@gmail.com>
2024-05-20 17:20:13 +0200

    Bump version: 0.0.3 → 0.0.4

--- vizbeauty/__init__.py
+++ vizbeauty/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Cristian Del Gobbo"""
 __email__ = "cristiandelgobbo87@gmail.com"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 from .common import print_statistic, pearson_correlation
 from .vizbeauty import beautybar, reg_scatter, visualize_hyperparameter
 
